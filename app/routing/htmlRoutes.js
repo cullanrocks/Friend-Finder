53caa5b1ec69d4e973d4726fa0448a014f937172
Cullan Shewfelt <cullanshewfelt@github.com>
2017-05-18 20:48:21 -0700

    working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var path = require('path');
+// var express = require('express');
+// var app = express();
+// var http = require('http');
+// var fs = require('fs');
+// var bodyParser = require('body-parser');
+// var path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static('app/'));
-app.use(express.static('app/public/'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(express.static('app/'));
+// app.use(express.static('app/public/'));
 
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-})
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "app/public/home.html"));
+// })
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-})
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "app/public/survey.html"));
+// })
 
